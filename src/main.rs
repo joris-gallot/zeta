b749564da459d30d945245f5b7ef291019cf58b3
Joris <joris.gallot18@gmail.com>
2025-12-30 17:52:34 +0100

    chore: move bind_keys to zeta

--- src/main.rs
+++ src/main.rs
@@ -5,42 +5,13 @@ use gpui::{
 mod text_element;
 mod text_input;
 mod zeta;
-use crate::{
-  text_input::{
-    AltLeft, AltRight, Backspace, CmdLeft, CmdRight, Copy, Cut, Delete, End, Home, Left, Paste,
-    Quit, Right, SelectAll, SelectEnd, SelectLeft, SelectRight, SelectStart, SelectWordLeft,
-    SelectWordRight, ShowCharacterPalette, TextInput,
-  },
-  zeta::Zeta,
-};
+use crate::{text_input::Quit, zeta::Zeta};
 
 fn main() {
   Application::new().run(|cx: &mut App| {
     let bounds = Bounds::centered(None, size(px(800.0), px(600.0)), cx);
 
-    cx.bind_keys([
-      KeyBinding::new("backspace", Backspace, None),
-      KeyBinding::new("delete", Delete, None),
-      KeyBinding::new("left", Left, None),
-      KeyBinding::new("alt-left", AltLeft, None),
-      KeyBinding::new("cmd-left", CmdLeft, None),
-      KeyBinding::new("right", Right, None),
-      KeyBinding::new("alt-right", AltRight, None),
-      KeyBinding::new("cmd-right", CmdRight, None),
-      KeyBinding::new("shift-left", SelectLeft, None),
-      KeyBinding::new("alt-shift-left", SelectWordLeft, None),
-      KeyBinding::new("cmd-shift-left", SelectStart, None),
-      KeyBinding::new("shift-right", SelectRight, None),
-      KeyBinding::new("alt-shift-right", SelectWordRight, None),
-      KeyBinding::new("cmd-shift-right", SelectEnd, None),
-      KeyBinding::new("cmd-a", SelectAll, None),
-      KeyBinding::new("cmd-v", Paste, None),
-      KeyBinding::new("cmd-c", Copy, None),
-      KeyBinding::new("cmd-x", Cut, None),
-      KeyBinding::new("home", Home, None),
-      KeyBinding::new("end", End, None),
-      KeyBinding::new("ctrl-cmd-space", ShowCharacterPalette, None),
-    ]);
+    Zeta::register(cx);
 
     let window = cx
       .open_window(
@@ -48,24 +19,7 @@ fn main() {
           window_bounds: Some(WindowBounds::Windowed(bounds)),
           ..Default::default()
         },
-        |_, cx| {
-          let text_input = cx.new(|cx| TextInput {
-            focus_handle: cx.focus_handle(),
-            content: "".into(),
-            placeholder: "Type here...".into(),
-            selected_range: 0..0,
-            selection_reversed: false,
-            marked_range: None,
-            last_layout: None,
-            last_bounds: None,
-            is_selecting: false,
-          });
-
-          cx.new(|cx| Zeta {
-            text_input,
-            focus_handle: cx.focus_handle(),
-          })
-        },
+        |_, cx| cx.new(Zeta::new),
       )
       .unwrap();
 
--- src/zeta.rs
+++ src/zeta.rs
@@ -1,10 +1,63 @@
-use gpui::{App, Context, Entity, FocusHandle, Focusable, Window, div, prelude::*, rgb};
+use gpui::{
+  App, Context, Entity, FocusHandle, Focusable, KeyBinding, Window, div, prelude::*, rgb,
+};
 
-use crate::text_input::TextInput;
+use crate::text_input::{
+  AltLeft, AltRight, Backspace, CmdLeft, CmdRight, Copy, Cut, Delete, End, Home, Left, Paste,
+  Right, SelectAll, SelectEnd, SelectLeft, SelectRight, SelectStart, SelectWordLeft,
+  SelectWordRight, ShowCharacterPalette, TextInput,
+};
 
 pub struct Zeta {
   pub text_input: Entity<TextInput>,
   pub focus_handle: FocusHandle,
+}
+
+impl Zeta {
+  pub fn new(cx: &mut Context<Self>) -> Self {
+    let text_input = cx.new(|cx| TextInput {
+      focus_handle: cx.focus_handle(),
+      content: "".into(),
+      placeholder: "Type here...".into(),
+      selected_range: 0..0,
+      selection_reversed: false,
+      marked_range: None,
+      last_layout: None,
+      last_bounds: None,
+      is_selecting: false,
+    });
+
+    Self {
+      text_input,
+      focus_handle: cx.focus_handle(),
+    }
+  }
+
+  pub fn register(cx: &mut App) {
+    cx.bind_keys([
+      KeyBinding::new("backspace", Backspace, None),
+      KeyBinding::new("delete", Delete, None),
+      KeyBinding::new("left", Left, None),
+      KeyBinding::new("alt-left", AltLeft, None),
+      KeyBinding::new("cmd-left", CmdLeft, None),
+      KeyBinding::new("right", Right, None),
+      KeyBinding::new("alt-right", AltRight, None),
+      KeyBinding::new("cmd-right", CmdRight, None),
+      KeyBinding::new("shift-left", SelectLeft, None),
+      KeyBinding::new("alt-shift-left", SelectWordLeft, None),
+      KeyBinding::new("cmd-shift-left", SelectStart, None),
+      KeyBinding::new("shift-right", SelectRight, None),
+      KeyBinding::new("alt-shift-right", SelectWordRight, None),
+      KeyBinding::new("cmd-shift-right", SelectEnd, None),
+      KeyBinding::new("cmd-a", SelectAll, None),
+      KeyBinding::new("cmd-v", Paste, None),
+      KeyBinding::new("cmd-c", Copy, None),
+      KeyBinding::new("cmd-x", Cut, None),
+      KeyBinding::new("home", Home, None),
+      KeyBinding::new("end", End, None),
+      KeyBinding::new("ctrl-cmd-space", ShowCharacterPalette, None),
+    ]);
+  }
 }
 
 impl Focusable for Zeta {
